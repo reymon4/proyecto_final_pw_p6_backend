60c97251a5fdce46fcd385defab21e46c82ef2ed
Alulestad <vladimirlestad@gmail.com>
2024-08-03 21:17:14 -0500

    base

---
+++ src/main/java/com/edu/uce/api/ProyectoFinalPwP6BackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPwP6BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPwP6BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/uce/api/ProyectoFinalPwP6BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalPwP6BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
